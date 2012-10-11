95328f1361b20542207ae20de60d586eb24179e5
Daniel Gilfoy <daniel@ivycat.com>
2012-10-11 16:26:33 -0700

    touch of security

--- posts-in-page/posts_in_page.php
+++ posts-in-page/posts_in_page.php
@@ -29,6 +29,12 @@
 
  */
 
+if (!function_exists ('add_action')) {
+        header('Status: 403 Forbidden');
+        header('HTTP/1.1 403 Forbidden');
+        exit();
+}
+
 define( 'POSTSPAGE_DIR', dirname( __FILE__ ) );
 define( 'POSTPAGE_URL', str_replace( ABSPATH, site_url( '/' ), POSTSPAGE_DIR ) );
 
