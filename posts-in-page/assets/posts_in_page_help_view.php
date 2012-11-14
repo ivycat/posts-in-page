dfecfe03f15b70a7a470d4c8615f76910c00841c
Eric Amundson <eric@ivycat.com>
2012-11-14 14:38:35 -0800

    Added wrapper with standard post classes around each post.  Updated Help.
    Signed-off-by: Eric Amundson <eric@ivycat.com>

--- posts-in-page/assets/posts_in_page_help_view.php
+++ posts-in-page/assets/posts_in_page_help_view.php
@@ -1,51 +1,49 @@
 <div class="wrap" id="posts-in-page-settings">
 <div id="icon-options-general" class="icon32"></div>
-    <h2>Posts in Page</h2>
-    <div id="body-wrap" class="meta-box-sortables ui-sortable">
-        <div id="metabox_desc" class="postbox">
-            <div class="handlediv" title="Click to toggle">
-                <br>
-            </div>
-            <div class="hndle">
-                <h3><ul class="top-menu clearfix">
-                    <li class="menu-item"><a href="#help">Help</a></li>
-                </ul></h3>
-            </div>
-            <div class="group help inside">
-                <ul>
-                    <li>
-                        <strong>[ic_add_posts]</strong>  - Add all posts to a page (limit to what number posts in WordPress is set to).  This essentially makes the page look like a blog.
-                    </li>
-                    <li>
-                        <strong>[ic_add_posts post_type='post_type' ids='1,2,3']</strong> - show posts with certain IDs (currently only one post type per category)
-                    </li>   
-                    <li>
-                        <strong>[ic_add_posts id='1']</strong> - show a single post with the given ID ( must give post type if not post )
-                    </li>    
-                    <li>
-                        <strong>[ic_add_posts showposts='5']</strong> - limit number of posts (or override default setting)
-                    </li>    
-                    <li>
-                        <strong>[ic_add_posts orderby='title' order='ASC']</strong> - orderby title - supports all WordPress orderby variables.  Order is optional, WP default
-                    </li>   
-                    <li>
-                        <strong>[ic_add_posts category='category-slug']</strong>  - Show posts within a specific category.  Uses slugs, and can have multiple categories separate by commas. category-1,category2, etc (no spaces.)
-                    </li>
-                    <li>
-                        <strong>[ic_add_posts post_type='post-type']</strong> - Show posts that belong to a specific post type (only one post type right now)
-                    </li>
-                    <li>
-                        <strong>[ic_add_posts tax='taxonomy' term='term']</strong> - limit posts to those that exist in a taxonomy and have a specific term.  Both are required for either one to work.
-                    </li>
-                    <li>
-                        <strong>[ic_add_posts template='template-in-theme-dir.php']</strong> - In case you want to style your markup, add meta data, etc.  Each shortcode can reference a different template.  These templates must exist in the theme directory.
-                        Or any combination above.
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    <div id="sidebar-wrap">
-            <?php require_once 'desc.php'; ?>
-    </div>
+	<h2>Posts in Page</h2>
+	<div id="body-wrap" class="meta-box-sortables ui-sortable">
+		<div id="metabox_desc" class="postbox">
+			<div class="handlediv" title="Click to toggle">
+				<br>
+			</div>
+			<div class="hndle">
+				<h3><ul class="top-menu clearfix">
+					<li class="menu-item"><a href="#help">Shortcode Usage</a></li>
+				</ul></h3>
+			</div>
+			<div class="group help inside">
+				<p>To 'pull' posts into a page, you can either:</p>
+
+				<ol>
+					<li>place a shortcode in the editor window of the page you're editing, or </li>
+					<li>modify a theme template file using the shortcode in a PHP function.</li>
+				</ol>
+
+				<h4>Using Shortcodes in the WordPress editor</h4>
+
+				<ul>
+					<li><code>[ic_add_posts]</code>  - Add all posts to a page (limit to what number posts in WordPress is set to), essentially adds blog "page" to page.</li>
+					<li><code>[ic_add_posts ids='1,2,3']</code> - show one or many posts by specifying the post ID(s) ( specify all post types )</li>
+					<li><code>[ic_add_posts post_type='post_type']</code> - show posts from a custom post type by specifying the post type slug ( must give post type if not a standard post ) add multiple post types by separating with commas (ex. <code>post_type='post_type1,post_type2'</code>)</li>
+					<li><code>[ic_add_posts showposts='5']</code> - limit number of posts (or override default setting)</li>
+					<li><code>[ic_add_posts orderby='title' order='ASC']</code> - orderby title - supports all WP orderby variables.  Order is optional, WP default </li>
+					<li><code>[ic_add_posts category='category-slug']</code> - Show posts within a specific category.  Uses slugs, can have multiple but separate by commas.      category-1,category2, etc (no spaces.)</li>
+					<li><code>[ic_add_posts tag='tag-slug']</code>  - Show posts using a specific tag.  Like categories, it uses slugs, and can accommodate multiple tags separate by commas.     tag-1,tag-2, etc (no spaces.)</li>
+					<li><code>[ic_add_posts tax='taxonomy' term='term']</code> - limit posts to those that exist in a taxonomy and have a specific term.  Both are required for either one to work</li>
+					<li><code>[ic_add_posts template='template-in-theme-dir.php']</code> - In case you want to style your markup, add meta data, etc.  Each shortcode can reference a different template.  These templates must exist in the theme directory.</li>
+				</ul>
+
+				<p>Or any combination of the above.</p>
+
+				<h4>Using Shortcodes within a PHP function</h4>
+
+				<p>If you'd like to use this plugin to pull posts directly into your theme's template files, you can drop the following WordPress function in your template files, replacing the <code>[shortcode]</code> part with your, custom shortcode.</p>
+
+				<pre><code>&lt;?php echo do_shortcode("[shortcode]"); ?&gt;</code></pre>
+			</div>
+		</div>
+	</div>
+	<div id="sidebar-wrap">
+		<?php require_once 'desc.php'; ?>
+	</div>
 </div>
--- posts-in-page/posts_loop_template.php
+++ posts-in-page/posts_loop_template.php
@@ -1,31 +1,35 @@
 <!-- Note: if you make changes to this file, move it to your current theme's
 	directory so this file won't be overwritten when the plugin is upgraded. -->
 
-<!-- This is the output of the post title -->
-<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+<!-- Start of Post Wrap -->
+<div class="post hentry ivycat-post">
+	<!-- This is the output of the post title -->
+	<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
-<!-- This is the output of the excerpt -->
-<div class="entry-summary">
-	<?php the_excerpt(); ?>
-</div>
+	<!-- This is the output of the excerpt -->
+	<div class="entry-summary">
+		<?php the_excerpt(); ?>
+	</div>
 
-<!-- This is the output of the meta information -->
-<div class="entry-utility">
-	<?php if ( count( get_the_category() ) ) : ?>
-		<span class="cat-links">
-			<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?>
-		</span>
-		<span class="meta-sep">|</span>
-	<?php endif; ?>
-	<?php
-		$tags_list = get_the_tag_list( '', ', ' );
-		if ( $tags_list ):
-	?>
-		<span class="tag-links">
-			<?php printf( __( '<span class="%1$s">Tagged</span> %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list ); ?>
-		</span>
-		<span class="meta-sep">|</span>
-	<?php endif; ?>
-	<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyten' ), __( '1 Comment', 'twentyten' ), __( '% Comments', 'twentyten' ) ); ?></span>
-	<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
+	<!-- This is the output of the meta information -->
+	<div class="entry-utility">
+		<?php if ( count( get_the_category() ) ) : ?>
+			<span class="cat-links">
+				<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?>
+			</span>
+			<span class="meta-sep">|</span>
+		<?php endif; ?>
+		<?php
+			$tags_list = get_the_tag_list( '', ', ' );
+			if ( $tags_list ):
+		?>
+			<span class="tag-links">
+				<?php printf( __( '<span class="%1$s">Tagged</span> %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list ); ?>
+			</span>
+			<span class="meta-sep">|</span>
+		<?php endif; ?>
+		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyten' ), __( '1 Comment', 'twentyten' ), __( '% Comments', 'twentyten' ) ); ?></span>
+		<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
+	</div>
 </div>
+<!-- // End of Post Wrap -->
